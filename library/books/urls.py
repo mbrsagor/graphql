b6e9e9a8a48bf19efa44d9eba41e151fc6d331c1
Sagor <mbrsagor@gmail.com>
2020-12-07 13:45:49 +0600

    Book retrive, update, annd delete API created

--- library/books/urls.py
+++ library/books/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BookAPIView, CreateBookAPIView, UpdateBookAPIView
+from .views import BookAPIView, CreateBookAPIView, UpdateBookAPIView, DetailBookAPIView, DeleteBookAPIView
 
 urlpatterns = [
     path('books/', BookAPIView.as_view(), name='books'),
     path('create-book/', CreateBookAPIView.as_view(), name='create_book'),
-    path('update-book/', UpdateBookAPIView.as_view(), name='update_book'),
+    path('update-book/<int:pk>/', UpdateBookAPIView.as_view(), name='update_book'),
+    path('book/<int:pk>/', DetailBookAPIView.as_view(), name='book_details'),
+    path('delete-book/<int:pk>/', DeleteBookAPIView.as_view(), name='delete_book'),
 ]
--- library/books/views.py
+++ library/books/views.py
@@ -9,13 +9,21 @@ class BookAPIView(generics.ListAPIView):
     serializer_class = BookSerializer
 
 
-
 class CreateBookAPIView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
-class UpdateBookAPIView(generics.UpdateAPIView):
+class UpdateBookAPIView(generics.UpdateAPIView, generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    lookup_field = id
+
+
+class DetailBookAPIView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class DeleteBookAPIView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
