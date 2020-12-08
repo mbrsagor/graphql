c910a54a9c0c97a4d47f9b61a9061f16e58fbd9a
Sagor <mbrsagor@gmail.com>
2020-12-08 22:23:22 +0600

    Default sqlite database unused code remove

--- library/settings.py
+++ library/settings.py
@@ -88,15 +88,6 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
