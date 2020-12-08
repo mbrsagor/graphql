1a1a299c343922350211695d73ebe082da25252a
Sagor <mbrsagor@gmail.com>
2020-12-08 12:12:01 +0600

    Django default rest api user login done

--- library/settings.py
+++ library/settings.py
@@ -147,5 +147,6 @@ CORS_ORIGIN_WHITELIST = (
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAuthenticated',
     ]
 }
--- library/urls.py
+++ library/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
     path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # new
 ]
