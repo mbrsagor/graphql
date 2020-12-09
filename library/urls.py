515aa2440da66aeb9bd757ed8d02d1b5a6b3160c
Sagor <mbrsagor@gmail.com>
2020-12-09 21:34:34 +0600

    Schema implement

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Blog API')  # new
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
@@ -8,4 +12,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('schema/', schema_view),
 ]
