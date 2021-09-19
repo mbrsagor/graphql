fa2917be33f5258ad5f490cfb10778e9f700d5c3
mbrsagor <mbrsagor@gmail.com>
2021-09-19 13:11:20 +0600

    Resolve: Fixed all & update GraphQL query

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 export let users = [
-    { id: 1, name: "John Doe", email: "john@gmail.com", age: 22 },
-    { id: 2, name: "Jane Doe", email: "jane@gmail.com", age: 23 }
+    { id: 1, name: "Rakibul-Alam", email: "rakib@gmail.com", age: 32 },
+    { id: 2, name: "Mbr-Sagor", email: "mbrsagor@gmail.com", age: 23 },
+    { id: 3, name: "Mozzammel-Haque", email: "mozzammel@gmail.com", age: 24 },
+    { id: 4, name: "Zarif-Rahaman", email: "zarif@gmail.com", age: 25 },
+    { id: 5, name: "Lokman-Hossain", email: "lokman@gmail.com", age: 26 }
   ];
-  
--- src/resolvers.js
+++ src/resolvers.js
@@ -2,7 +2,37 @@ import { users } from "./db";
 
 const resolvers = {
   Query: {
-    hello: () => "Hello World!"
+    user: (parent, { id }, context, info) => {
+      return users.find(user => user.id === id);
+    },
+    users: (parent, args, context, info) => {
+      return users;
+    }
+  },
+  Mutation: {
+    createUser: (parent, { id, name, email, age }, context, info) => {
+      const newUser = { id, name, email, age };
+      users.push(newUser);
+
+      return newUser;
+    },
+    updateUser: (parent, { id, name, email, age }, context, info) => {
+      let newUser = users.find(user => user.id === id);
+      newUser.name = name;
+      newUser.email = email;
+      newUser.age = age;
+
+      return newUser;
+    },
+    deleteUser: (parent, { id }, context, info) => {
+      const userIndex = users.findIndex(user => user.id === id);
+
+      if (userIndex === -1) throw new Error("User not found.");
+
+      const deletedUsers = users.splice(userIndex, 1);
+
+      return deletedUsers[0];
+    }
   }
 };
 
