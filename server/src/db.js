d64fc297bd415e2173e3df6f4184e7a1641b45be
mbrsagor <mbrsagor@gmail.com>
2024-01-27 21:51:52 +0600

    Refactor: error message refactor.

--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,7 @@
 export let users = [
     { id: 1, name: "Rakibul-Alam", email: "rakib@gmail.com", age: 32 },
-    { id: 2, name: "Mbr-Sagor", email: "mbrsagor@gmail.com", age: 23 },
+    { id: 2, name: "Bozlur Rosid Sagor", email: "mbrsagor@gmail.com", age: 23 },
     { id: 3, name: "Mozzammel-Haque", email: "mozzammel@gmail.com", age: 24 },
     { id: 4, name: "Zarif-Rahaman", email: "zarif@gmail.com", age: 25 },
-    { id: 5, name: "Lokman-Hossain", email: "lokman@gmail.com", age: 26 }
+    { id: 5, name: "Nayeem-Abir", email: "abir.cse@gmail.com", age: 26 }
   ];
